dfab5c23d3ecdf5b1aa89e1a5fb334d74273374c
dhruv-bhatia246 <71057049+dhruv-bhatia246@users.noreply.github.com>
2021-05-04 17:30:21 +0530

    Add files via upload

---
+++ src/Components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+// import 'tachyons';
+import "./Navbar.css";
+
+const Navbar = () =>{
+    return (
+        <nav>
+        <ul>
+            <li><a href="#">Shop</a></li>
+            <li><a href="#">Read</a></li>
+            <li><a href="#">Stores</a></li>
+            <li><a href="#">Search</a></li>
+        </ul>
+        </nav>
+    )
+}
+
+export default Navbar;
